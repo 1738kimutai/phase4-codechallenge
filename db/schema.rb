95a20fbd2bfeca0a86afa326ee496f2a72b8ea5d
1738kimutai <davieskimutai4@gmail>
2023-03-21 14:44:49 +0300

    done

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_100724) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "address"
     t.string "ingredients"
   end
 
